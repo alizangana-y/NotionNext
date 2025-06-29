ae0985e8ec4d301272bb32dcb7f6531e94800b81
alizangana-y <ruyuyan08@gmail.com>
2025-06-29 11:00:41 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -98,8 +98,8 @@ CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || '', // Youtube主页
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || false, // 是否显示宠物挂件
   WIDGET_PET_LINK:
       process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-      'https://cdn.jsdelivr.net/npm/live2d-widget-model-wanko@1.0.5/assets/wanko.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
-  WIDGET_PET_SWITCH_THEME: true, // 点击宠物挂件切换博客主题
+      '', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
+  WIDGET_PET_SWITCH_THEME: false, // 点击宠物挂件切换博客主题
   
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
